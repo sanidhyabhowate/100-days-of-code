b8814c636964657553cdd49c41a0e2771c74de26
Sanidhya Bhowate <sanidhyavb06@gmail.com>
2026-06-24 17:53:09 +0530

    Updated grade checker with if elif else

--- grade_checker.py
+++ grade_checker.py
@@ -1,6 +1,11 @@
 marks = int(input("Enter your marks: "))
 
-if marks >=40:
+if marks >= 75:
+    print("You have passed the exam with distinction")
+
+elif marks >= 60:
+    print("Yo have passed the exam with first class")
+elif marks >=40:
     print("You have passed the exam.")
 else:
     print("You have failed the exam.")
