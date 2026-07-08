c4d41c3c31e08ac1f477bfcbd9a06382d802efe0
Sanidhya Bhowate <sanidhyavb06@gmail.com>
2026-07-08 19:17:40 +0530

    Reviewed calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,17 @@
-num1 = int(input("Enter first number: "))
+# taking first number from user
+num1 = int(input("Enter first number: ")) 
+
+# taking second number from user
 num2 = int(input("Enter second number: "))
 
+# printing the sum of both numbers
 print("Sum =", num1 + num2)
+
+# printing the difference of both numbers
 print("Difference =", num1 - num2)
+
+# printing the product of both numbers
 print("Product =", num1 * num2)
+
+# printing the quotient of both numbers
 print("Quotient =", num1 / num2)
