487ed505978717a286118683295351948de72463
Sanidhya Bhowate <sanidhyavb06@gmail.com>
2026-08-10 17:24:26 +0530

    Day 17 - Python re-entry practice

---
+++ day17_reentry.py
@@ -0,0 +1,49 @@
+
+
+age = int(input("What is your age:"))
+
+if age >= 18:
+    print("You are eligible to vote")
+
+else:
+    print("You are underage")
+
+
+
+
+number = int(input("Choose a number:"))
+
+for i in range(1,11):
+    print(number*i)
+
+
+
+for i in range(1, 6):
+    number = int(input("Choose a number:"))
+
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+
+for i in range(1,6):
+    number = int(input("Choose a number:"))
+
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+    if number < 0:
+        print("negative")
+    elif number > 0:
+        print("Positive")
+    else:
+        print("Zero")
+    
+
+
+        
+    
